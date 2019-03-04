859f9469cf2da7115afdd060c25ce9044703ae9b
user <user@DESKTOP-VSR60N5>
2019-03-04 17:22:36 +0900

    문의사항

--- ResearchBank/src/main/java/com/kh/researchbank/Crm/Inquiry/service/Impl/InquiryServiceImpl.java
+++ ResearchBank/src/main/java/com/kh/researchbank/Crm/Inquiry/service/Impl/InquiryServiceImpl.java
@@ -1,7 +1,13 @@
 package com.kh.researchbank.Crm.Inquiry.service.Impl;
 
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Service;
 
+import com.kh.researchbank.Crm.Inquiry.dao.InquiryDAO;
 import com.kh.researchbank.Crm.Inquiry.service.InquiryService;
 
 /**
@@ -23,17 +29,58 @@ import com.kh.researchbank.Crm.Inquiry.service.InquiryService;
 @Service("inquiryService")
 public class InquiryServiceImpl implements InquiryService{
 
+	@Resource
+	private InquiryDAO inquiryDAO;
+	
+	@Override
+	   public String index() throws Exception {
+	      String forward = "/crm/inquiry/index";
+	      return forward;
+	   }
+
+	@Override
+	public List<Map<String, Object>> show(Map<String, Object> map) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println(map);
+		return inquiryDAO.show(map);
+		
+	}
+
+	@Override
+	public void creat(Map<String, Object> map) throws Exception {
+		// TODO Auto-generated method stub
+		inquiryDAO.create(map);
+		
+	}
+
+	@Override
+	public Map<String, Object> showDetail(Map<String, Object> map) throws Exception {
+		// TODO Auto-generated method stub
+		inquiryDAO.updateHitCnt(map);
+		Map<String,Object> resultMap = inquiryDAO.showDetail(map);
+		return resultMap;
+	}
+
+	@Override
+	public void update(Map<String, Object> map) throws Exception {
+		// TODO Auto-generated method stub	
+		inquiryDAO.update(map);
+		
+	}
+
+	@Override
+	public void delete(Map<String, Object> map) throws Exception {
+		// TODO Auto-generated method stub
+		inquiryDAO.delete(map);
+	}
+
 	
 	/**********
 	 * @title 1:1문의 목록 
 	 * @return
 	 * @throws Exception
 	 */
-	@Override
-	public String index() throws Exception {
-		String forward = "/crm/inquiry/index";
-		return forward;
-	}
+
 
 	
 }
